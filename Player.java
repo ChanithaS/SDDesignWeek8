1beb985d7434c0ceff8e5908af2190b22822177a
ChanithaS <chanitha.sankalpana3@gmail.com>
2021-07-07 07:43:13 +0530

    1st push

--- Player.java
+++ Player.java
@@ -1,2 +1,24 @@
+package com.company;
+
+
 public class Player {
+	
+    private static String Davd_Malan;
+
+    private static String Tabraiz_ShamSi;
+
+    private static String Mohomad_Nabi;
+
+    private static String Umar_Gul;
+
+    private static String Shane_Watson;
+
+
+
+    public static void main(String[] args) {
+
+String players[]={Davd_Malan,Tabraiz_ShamSi,Mohomad_Nabi,Umar_Gul,Shane_Watson}
+
+    }
+
 }
