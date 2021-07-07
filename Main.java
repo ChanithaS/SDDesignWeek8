d1aca41ec8e0a70f3af800ddd16737adb4d85fb8
tharuka2000 <81518879+tharuka2000@users.noreply.github.com>
2021-07-07 16:25:39 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        public static  battingORBallingAvg(){
+
+        }
+        public static string playerDetails{
+            int i =0;
+
+            return player[i];
+            i++
+        }
+        public static string playerType(){
+
+        }
+    }
+}
