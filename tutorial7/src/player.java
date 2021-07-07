8c29d1a87a13ddad960898920c8ac36a110b1ffb
Tihara-Jay <86401187+Tihara-Jay@users.noreply.github.com>
2021-07-07 15:22:53 +0530

    Add files via upload
    Task 1 part 1 (Creating the player class and using the constructor to pass the data) - Tihara Jayawickrama

---
+++ tutorial7/src/player.java
@@ -0,0 +1,13 @@
+public class player {
+    private String playerName;
+    private int playerAge;
+    private String playerType;
+    private double statistics;
+
+    public player (String playerName, int playerAge, String playerType, double statistics){
+        this.playerName = playerName;
+        this.playerAge = playerAge;
+        this.playerType = playerType;
+        this.statistics = statistics;
+    }
+}
